dfdfb3e902a6d5921fe6f4258ffa29767a46e8ac
Ignacio Vargas <ignaevc@gmail.com>
2022-09-10 15:33:25 -0600

    ADD: Parse arguments now affect the behaviour of the program

--- proyecto_1/src/main.rs
+++ proyecto_1/src/main.rs
@@ -16,11 +16,7 @@ use winit::{
 mod interpolation;
 mod utils;
 
-const IMAGE: &str = "input.jpg";
-const RESULT: &str = "result.img";
-const N: u32 = 4;
-
-#[derive(Parser, Debug)]
+#[derive(Parser, Clone, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Input image to use
@@ -40,7 +36,7 @@ struct Args {
 
     /// divide into n^2 squares
     #[clap(short, default_value_t = 4)]
-    n: u8,
+    n: u32,
 }
 
 enum ImageState {
@@ -48,51 +44,55 @@ enum ImageState {
     AfterSelection(GrayImage),
 }
 
-fn main() -> Result<()> {
-    let _args = Args::parse();
+struct Context {
+    args: Args,
+    graphics_context: GraphicsContext<Window>,
+    img_state: ImageState,
+    mouse_pos: PhysicalPosition<f64>,
+}
 
-    let gray_img: GrayImage = grayscale(&ImageReader::open(IMAGE)?.decode()?);
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let gray_img: GrayImage = grayscale(&ImageReader::open(&args.input)?.decode()?);
     gray_img.save("input.png").unwrap();
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-    let mut graphics_context: GraphicsContext<Window> =
-        unsafe { GraphicsContext::new(window) }.unwrap();
 
-    let mut mouse_pos = PhysicalPosition::<f64>::new(0.0, 0.0);
-
-    let mut img_state: ImageState = ImageState::BeforeSelection(gray_img);
+    let mut context = Context {
+        args,
+        graphics_context: unsafe { GraphicsContext::new(window) }.unwrap(),
+        img_state: ImageState::BeforeSelection(gray_img),
+        mouse_pos: PhysicalPosition::<f64>::new(0.0, 0.0),
+    };
 
     event_loop.run(move |event, _, control_flow| {
         control_flow.set_wait();
 
         let (win_width, win_height) = {
-            let size = graphics_context.window().inner_size();
+            let size = context.graphics_context.window().inner_size();
             (size.width, size.height)
         };
 
         match event {
-            Event::RedrawRequested(window_id) if window_id == graphics_context.window().id() => {
-                handle_redraw(&mut graphics_context, &img_state, win_height, win_width);
+            Event::RedrawRequested(window_id)
+                if window_id == context.graphics_context.window().id() =>
+            {
+                handle_redraw(&mut context, win_height, win_width);
             }
 
             Event::WindowEvent { event, window_id }
-                if window_id == graphics_context.window().id() =>
+                if window_id == context.graphics_context.window().id() =>
             {
                 match event {
                     WindowEvent::CloseRequested => control_flow.set_exit(),
                     WindowEvent::CursorMoved { position, .. } => {
-                        mouse_pos = position;
+                        context.mouse_pos = position;
                     }
-                    WindowEvent::MouseInput { state, button, .. } => handle_input(
-                        &graphics_context,
-                        &mut img_state,
-                        mouse_pos,
-                        win_height,
-                        win_width,
-                        state,
-                        button,
-                    ),
+                    WindowEvent::MouseInput { state, button, .. } => {
+                        handle_input(&mut context, win_height, win_width, state, button)
+                    }
                     _ => {}
                 }
             }
@@ -101,58 +101,61 @@ fn main() -> Result<()> {
     });
 }
 
-fn handle_redraw(
-    graphics_context: &mut GraphicsContext<Window>,
-    img_state: &ImageState,
-    height: u32,
-    width: u32,
-) {
-    match &img_state {
+fn handle_redraw(context: &mut Context, win_height: u32, win_width: u32) {
+    match &context.img_state {
         ImageState::BeforeSelection(img) => {
-            let res_img = resize(img, width, height, FilterType::Nearest);
+            let res_img = resize(img, win_width, win_height, FilterType::Nearest);
 
             let buffer: Vec<u32> = utils::gray_to_vec32(res_img);
-            let buffer = utils::draw_grid(buffer, width, height, N).unwrap();
+            let buffer = utils::draw_grid(buffer, win_width, win_height, context.args.n).unwrap();
 
-            graphics_context.set_buffer(&buffer, width as u16, height as u16);
+            context
+                .graphics_context
+                .set_buffer(&buffer, win_width as u16, win_height as u16);
         }
         ImageState::AfterSelection(img) => {
-            let res_img = resize(img, width, height, FilterType::Nearest);
+            let res_img = resize(img, win_width, win_height, FilterType::Nearest);
 
             let buffer: Vec<u32> = utils::gray_to_vec32(res_img);
-            graphics_context.set_buffer(&buffer, width as u16, height as u16);
+            context
+                .graphics_context
+                .set_buffer(&buffer, win_width as u16, win_height as u16);
         }
     }
 }
 
 fn handle_input(
-    graphics_context: &GraphicsContext<Window>,
-    img_state: &mut ImageState,
-    mouse_pos: PhysicalPosition<f64>,
-    height: u32,
-    width: u32,
+    context: &mut Context,
+    win_height: u32,
+    win_width: u32,
     state: ElementState,
     button: MouseButton,
 ) {
     if state == ElementState::Pressed && button == MouseButton::Left {
-        if let ImageState::BeforeSelection(img) = &img_state {
-            let square: u32 = utils::check_grid(mouse_pos.x, mouse_pos.y, width, height, N);
-            let square_width: f64 = img.width() as f64 / N as f64;
-            let square_height: f64 = img.height() as f64 / N as f64;
+        if let ImageState::BeforeSelection(img) = &context.img_state {
+            let square: u32 = utils::check_grid(
+                context.mouse_pos.x,
+                context.mouse_pos.y,
+                win_width,
+                win_height,
+                context.args.n,
+            );
+            let square_width: f64 = img.width() as f64 / context.args.n as f64;
+            let square_height: f64 = img.height() as f64 / context.args.n as f64;
 
-            let x: u32 = ((square % N) as f64 * square_width).round() as u32;
-            let y: u32 = ((square / N) as f64 * square_height).round() as u32;
+            let x: u32 = ((square % context.args.n) as f64 * square_width).round() as u32;
+            let y: u32 = ((square / context.args.n) as f64 * square_height).round() as u32;
             let cut_image =
                 utils::cut_image(img, x, y, x + square_width as u32, y + square_width as u32);
 
             interpolation::bil_interpol(
-                RESULT,
+                &context.args.intermediate,
                 cut_image.as_raw(),
                 cut_image.height(),
                 cut_image.width(),
             );
 
-            let interpolated_buffer = std::fs::read(RESULT).unwrap();
+            let interpolated_buffer = std::fs::read(&context.args.intermediate).unwrap();
 
             let interpolated_image: GrayImage = ImageBuffer::from_vec(
                 cut_image.width() * 3 - 2,
@@ -161,11 +164,13 @@ fn handle_input(
             )
             .unwrap();
 
-            interpolated_image.save("result.png").unwrap();
+            if let Some(str) = &context.args.gray {
+                interpolated_image.save(&str).unwrap();
+            }
 
-            *img_state = ImageState::AfterSelection(interpolated_image);
+            context.img_state = ImageState::AfterSelection(interpolated_image);
 
-            graphics_context.window().request_redraw();
+            context.graphics_context.window().request_redraw();
         }
     }
 }
